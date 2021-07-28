aac23ca3251fed96ce20dd989cfa0666ff91d4da
Andy Wade <carrollandywadejr@gmail.com>
2021-07-28 14:17:46 -0500

    completed the models seriaizer

--- sewproject/flashcards/admin.py
+++ sewproject/flashcards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Collection
+from .models import Flashcard
 
 # Register your models here.
+admin.site.register(Collection)
+admin.site.register(Flashcard)
--- sewproject/sewproject/settings.py
+++ sewproject/sewproject/settings.py
@@ -78,12 +78,6 @@ WSGI_APPLICATION = 'sewproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
