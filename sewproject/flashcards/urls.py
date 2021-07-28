9eaaae9178050567408667d197293e3270c14b05
Andy Wade <carrollandywadejr@gmail.com>
2021-07-28 15:11:14 -0500

    added a get all collections method and url

---
+++ sewproject/flashcards/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('collections/', views.CollectionList.as_view(), name='collections'),
+
+]
--- sewproject/flashcards/views.py
+++ sewproject/flashcards/views.py
@@ -3,6 +3,25 @@ from models import Collection, Flashcard
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import CollectionSerializer
+from .serializers import FlashcardSerializer
 
 
 # Create your views here.
+class CollectionList(APIView):
+    def get(self, request):
+        collection = Collection.objects.all()
+        serializer = CollectionSerializer(collection, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CollectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FlashcardList(APIView):
+    def get(self, request):
+        pass
